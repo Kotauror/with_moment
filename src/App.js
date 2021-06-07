2b86ba2b917fc73bba309f694abe2c025af5bfbd
Kotauror <zygmunt.justyna@gmail.com>
2021-06-07 15:36:53 +0100

    Add moment and moment timezone, use them.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import moment from "moment-timezone"
 
 function App() {
   return (
@@ -16,10 +17,23 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          {formatDate("2021-05-06T10:00")}
         </a>
       </header>
     </div>
   );
 }
+
+function formatDate(dateTime) {
+  return moment(parseReflexUtcDateTime(dateTime))
+    .tz("America/Chicago")
+    .format("MM/DD/YYYY H:mmA z")
+}
+
+function parseReflexUtcDateTime(dateTime) {
+  const b = dateTime.split(/\D/)
+  return new Date(Date.UTC(b[0], b[1] - 1, b[2], b[3], b[4]))
+}
+
 
 export default App;
